02081d4310699145e1af838d9b3d9a7928da4881
oblachko88 <88oblachko@gmail.com>
2024-08-17 17:27:19 +0300

    Commit Aug 17

--- Documents/Javascipt_f0r_begginers/index.js
+++ Documents/Javascipt_f0r_begginers/index.js
@@ -1,18 +1,43 @@
-const myName = 'Danila'
+// const myName = 'Danila'
 
 
-console.log(myName)
+// console.log(myName)
 
 
-const objectA = {
-  a: 10,
-  b: true
+// const objectA = {
+//   a: 10,
+//   b: true
+// }
+
+// const copyOfA = objectA
+// copyOfA.a = 20
+// copyOfA.c = 'abc'
+// // Added .c
+
+// // console.log(copyOfA)
+// console.log(copyOfA)
+
+// const myCity = {
+//   city: 'New York', 
+// }
+
+// myCity.popular = true
+// console.log(myCity)
+
+// myCity.country = 'USA'
+// console.log(myCity)
+
+// delete myCity.country
+// console.log(myCity)
+
+const person = {
+  name: 'Bob',
+  age: 25
 }
 
-const copyOfA = objectA
-copyOfA.a = 20
-copyOfA.c = 'abc'
-// Added .c
+const person2 = Object.assign({}, person)
 
-// console.log(copyOfA)
-console.log(copyOfA)
+person2.age = 26
+
+console.log(person2.age) // 26
+console.log(person.age) // 25 
