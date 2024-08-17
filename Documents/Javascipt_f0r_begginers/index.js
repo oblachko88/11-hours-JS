1543cc789ca50785063e39f0d877c3332b396e02
oblachko88 <88oblachko@gmail.com>
2024-08-17 19:28:50 +0300

    Commit Aug 17

--- Documents/Javascipt_f0r_begginers/index.js
+++ Documents/Javascipt_f0r_begginers/index.js
@@ -30,14 +30,64 @@
 // delete myCity.country
 // console.log(myCity)
 
-const person = {
+// const person = {
+//   name: 'Bob',
+//   age: 25
+// }
+
+// // const person2 = Object.assign({}, person)
+// // const person2 = {...person}
+// const person2 = JSON.parce(JSON.stringify(person))
+
+// person2.name = 'Alice'
+
+
+// console.log(person2.name) // 26
+// console.log(person.name) // 25
+
+// let a = 5
+// let b = 3
+
+// let c
+
+// // c = a + b
+// // console.log(c) // 8
+
+
+// // c = a + b 
+// // console.log(c) // 20
+
+// function sum(a, b) {
+//   const c = a + b 
+//   console.log(c)
+// }
+
+// sum(a, b) // 8
+
+// a = 8 
+// b = 12 
+
+// sum(a, b) // 20
+
+// function myFn(a, b) {
+//   let c
+//   a = a + 1
+//   c = a + b
+//   return c
+// }
+
+
+// console.log(myFn(10, 3))
+
+const person0ne = {
   name: 'Bob',
-  age: 25
+  age: 21
 }
 
-const person2 = Object.assign({}, person)
+function increasePersonAge(person) {
+  person.age += 1 
+  return person
+}
 
-person2.age = 26
-
-console.log(person2.age) // 26
-console.log(person.age) // 25 
+increasePersonAge(person0ne)
+console.log(person0ne.age)
